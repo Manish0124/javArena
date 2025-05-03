1f9fae55ceead1139de1973741ade536135fb1aa
Manish <techno8427@gmail.com>
2025-05-04 01:39:21 +0530

    chore: encaptulate added

---
+++ oops/part3/encaptulation/Encaptulation.java
@@ -0,0 +1,9 @@
+package oops.part3.encaptulation;
+
+public class Encaptulation {
+    //what is encapsulation ?
+    //wrapping up the implementation of the data member and the method in a class
+    
+    //what is abstration
+    // hidding unneccessary details and sharing only valueable information
+}
