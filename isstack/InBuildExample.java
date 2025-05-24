661bb8e562a8ea61458f27feffb74608ea2acab9
Manish <techno8427@gmail.com>
2025-05-24 21:53:30 +0530

    chore : stack added

---
+++ isstack/InBuildExample.java
@@ -0,0 +1,31 @@
+package isstack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class InBuildExample {
+
+    public static void main(String[] args) {
+        // Stack<Integer> stack = new Stack<>();
+
+        // stack.push(12);
+        // stack.push(15);
+        // stack.push(19);
+        // stack.push(16);
+
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+       
+        
+        Queue <Integer> queue = new LinkedList<>();
+
+        queue.add(12);
+        queue.add(13);
+        queue.add(14);
+        queue.add(15);
+       System.out.println(queue.remove());
+    }
+}
