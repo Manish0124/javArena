442c81c0c01dc7f66d674f3807e3d886511fd89d
Manish <techno8427@gmail.com>
2025-05-27 00:50:28 +0530

    stack q started

---
+++ qonstack/QueueUsingStack.java
@@ -0,0 +1,35 @@
+package qonstack;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+
+  Stack<Integer> stack = new Stack<>();
+    
+    private Stack<Integer>  first;
+    private Stack<Integer>  second;
+
+
+    public QueueUsingStack(){
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+
+    public void add(int item){
+        first.push(item);
+    }
+
+    public int remove() throws Exception {
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+
+        int removed = second.pop();
+
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+
+        return removed;
+    }
+}
---
+++ qonstack/question.java
@@ -0,0 +1,16 @@
+package qonstack;
+
+import java.util.Stack;
+
+public class question {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        // stack.add(12);
+        // stack.add(13);
+        // stack.add(14);
+
+        // System.out.println(stack.pop());
+    }
+}
