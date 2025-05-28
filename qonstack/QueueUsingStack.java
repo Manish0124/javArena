aeebb3e1ea686758127522553a05730820ef1f97
Manish <techno8427@gmail.com>
2025-05-28 21:20:54 +0530

    queue qs added

--- qonstack/QueueUsingStack.java
+++ qonstack/QueueUsingStack.java
@@ -32,4 +32,23 @@ public class QueueUsingStack {
 
         return removed;
     }
+
+
+    public int peek() throws Exception {
+       while (!first.isEmpty()) {
+         second.push(first.pop());
+       }
+
+       int peeked = second.pop();
+
+       while (!second.isEmpty()) {
+         first.push(second.pop());
+       }
+
+       return peeked;
+    }
+
+    public boolean isEmpty(){
+        return first.isEmpty();
+    }
 }
